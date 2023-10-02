a5c914a6bb0351d4cd69558a486e2af7d6763d78
Hassan Dannyal <100085970+Spectra-Ph@users.noreply.github.com>
2023-10-02 02:56:41 -0400

    Create ProjectList.js

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,21 @@
+import Proj2 from "../assets/G-rack.png";
+import Proj3 from "../assets/Portfolio_ss.png";
+import Proj1 from "../assets/Potential_Graide.png";
+
+export const ProjectList = [
+  {
+    name: "GRAIDE",
+    image: Proj1,
+    skills: "FastAPI, React, OpenAIAPI, TensorFlow, Py, Js",
+  },
+  {
+    name: "Gack",
+    image: Proj2,
+    skills: "React,Node.js,MongoDB,Express,GoogleMapsAPI, Js",
+  },
+  {
+    name: "Portfolio",
+    image: Proj3,
+    skills: "React,Node.js,Javascript",
+  },
+];
